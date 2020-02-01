1c133914aeddabb316bbaeb1b65fde16b465b5bb
martin123kupec <47635015+martin123kupec@users.noreply.github.com>
2020-02-01 22:15:48 +0100

    Add files via upload

---
+++ vue_simple/js/main.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        newDude: '',
+        characters: [
+            'Jake the Dog',
+            'Finn the Human',
+            'Marceline the Vampire'
+
+        ]
+    }
+})
